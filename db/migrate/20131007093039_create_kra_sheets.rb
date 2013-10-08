e047f944c794127e61fd5f48909b66dd187a7fa7
Kummitha Murali <kummitha@neevtech.com>
2013-10-08 11:17:29 +0530

    modifications to the KRA sheet DB and UI

--- db/migrate/20131007093039_create_kra_sheets.rb
+++ db/migrate/20131007093039_create_kra_sheets.rb
@@ -4,7 +4,8 @@ class CreateKraSheets < ActiveRecord::Migration
     	t.belongs_to :appraisal_cycle
     	t.belongs_to :appraisee, :foreign_key => 'appraisee_id', :class_name => "User"
   		t.belongs_to :appraiser, :foreign_key => 'appraiser_id', :class_name => "User"
-
+  		t.boolean :appraisee_status,:default => false
+  		t.boolean :appraiser_status, :default => false
       t.timestamps
     end
   end
