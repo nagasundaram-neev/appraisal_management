ae6d5e74caf9fd60b3b10a11c9df37e4d589a35f
Prashant Singla <sprashant@neevtech.com>
2013-10-11 16:12:01 +0530

    post login for appraiser

--- app/helpers/kra_ratings_helper.rb
+++ app/helpers/kra_ratings_helper.rb
@@ -15,7 +15,7 @@ module KraRatingsHelper
 
   def kr_attr_comment(kr_sheet_id, kr_attr_id, rated_by)
     begin
-    @kr_atr_comment  KraRating.select("comment").where(:kra_sheet_id => kr_sheet_id, :kra_attr_id => kr_attr_id, :rated_by => rated_by).first.comment
+    @kr_atr_comment = KraRating.select("comment").where(:kra_sheet_id => kr_sheet_id, :kra_attr_id => kr_attr_id, :rated_by => rated_by).first.comment
     return @kr_atr_comment
     rescue NoMethodError
       return nil
